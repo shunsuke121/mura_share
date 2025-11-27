b085462268fcc416d27062f3ebee4ef461b75dc8
shunsuke121 <user@wxample.com>
2025-11-27 12:16:05 +0900

    作業中のコミット

---
+++ marketplace/migrations/0006_remove_productfavorite_marketplace_product_396c08_idx.py
@@ -0,0 +1,5 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    dependencies = [("marketplace", "0005_productfavorite_alter_purchase_options_and_more")]
+    operations = []
